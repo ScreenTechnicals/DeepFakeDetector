220e330f76a2074a82415048604b82cccb32ee95
chsa <chsa@cisco.com>
2026-05-27 01:14:39 +0530

    inital commit

--- sdk/deepsafe_sdk/types.py
+++ sdk/deepsafe_sdk/types.py
@@ -7,3 +7,18 @@ class PredictionResult(BaseModel):
     prediction: int = Field(ge=0, le=1)
     class_name: str = Field(serialization_alias="class")
     inference_time: float
+
+    def model_dump(self, *args, **kwargs):
+        by_alias = kwargs.pop("by_alias", False)
+
+        if hasattr(BaseModel, "model_dump"):
+            data = BaseModel.model_dump(
+                self, *args, by_alias=by_alias, **kwargs
+            )
+        else:
+            data = self.dict(*args, by_alias=by_alias, **kwargs)
+
+        if by_alias and "class_name" in data and "class" not in data:
+            data["class"] = data.pop("class_name")
+
+        return data
